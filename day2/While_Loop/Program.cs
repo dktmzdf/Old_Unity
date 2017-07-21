10aa4a53284f809f9aa066d3c02a38d95eebc325
kujin <dktmzdf@gmail.com>
2017-07-21 15:33:28 +0900

    while_Loop

---
+++ day2/While_Loop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            //원래 모든 조건문 반복문은 한줄처ㅣ만 함 -수행하다
+            do
+            {//메모리 영역을 만들어줌 => 한개의 기능
+                int a = 2 * i + 1;
+                Console.WriteLine("값:" + a);//처리영역(기능수행영역 1줄)
+                i++;
+            } while (i < 8);
+
+
+        }
+    }
+}
