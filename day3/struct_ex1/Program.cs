31c859fc99d028eabcb559f7c0a30f0a59108743
kujin <dktmzdf@gmail.com>
2017-07-24 16:15:36 +0900

    struct_ex1

--- day3/struct_ex1/Program.cs
+++ day3/struct_ex1/Program.cs
@@ -44,11 +44,17 @@ namespace struct_ex1
             Hero ironman
             ironman.hp = 100;
             ironman.mp = 100;*/
+
+
+            //간접접근(저장공간을 만들고 해당 주소를 받음)
             Hero ironman = new Hero();//new Hero처럼 메모리 할당해주면 저장공간이 생기므로 함수사용가능해짐
-            //멤버를 직접 접근... 값세팅 -> 일반변수 처럼 사용됨
+            //Hero *ironman = (Hero *)malloc(sizeof(Hero));
             ironman.init_data();//내부 메서드, 내부 데이터를 변경
             ironman.print();//내부 메서드, 내부 데이터를 변경
 
+
+            //직접접근
+            //멤버를 직접 접근... 값세팅 -> 일반변수 처럼 사용됨
             Hero spiderman;
             spiderman.hp = 100;
             spiderman.mp = 100;
