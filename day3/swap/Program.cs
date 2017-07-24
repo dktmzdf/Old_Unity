208da46e99dfe1784339f0e6f6c4ba3a8685af62
kujin <dktmzdf@gmail.com>
2017-07-24 13:32:11 +0900

    swap

---
+++ day3/swap/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swap
+{
+    class Program
+    {
+        public static void Swap(int a, int b)
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+        public static void Swap(ref int a,ref int b)//ref 주소값
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 4;
+
+            Console.WriteLine("x:{0} y:{1}",x,y);
+
+            Swap(x, y);
+            Console.WriteLine("x:{0} y:{1}", x, y);
+            Swap(ref x,ref y);
+            Console.WriteLine("x:{0} y:{1}", x, y);
+
+        }
+    }
+}
