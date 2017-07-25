e01cce126568cb0272b98f43a8f712714c935e70
kujin <dktmzdf@gmail.com>
2017-07-25 14:34:09 +0900

    SF

---
+++ day4/SF/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Global
+{
+    public static int Count = 0;
+
+}
+
+class ClassA {
+    public ClassA()
+    {
+        Global.Count++;
+    }
+}
+class ClassB
+{
+    public ClassB()
+    {
+        Global.Count++;
+    }
+}
+
+namespace SF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Global.Count : {0}", Global.Count);
+            new ClassA();
+            new ClassA();
+            new ClassB();
+            new ClassB();
+
+            Console.WriteLine("Global.Count : {0}", Global.Count);
+            ClassA[] em = new ClassA[5];
+        }
+    }
+}
