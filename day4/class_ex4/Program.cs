9ec8d2e85151990d7d46067da8a1ce1f74bc46ea
kujin <dktmzdf@gmail.com>
2017-07-25 14:29:09 +0900

    class_ex4

--- day4/class_ex4/Program.cs
+++ day4/class_ex4/Program.cs
@@ -3,6 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*클래스내에 정적 자료형을 만들었을때
+*형식
+* 클래스{
+* 정적 자료형의 선언과 초기화
+* }
+* 사용
+* 클래스.선언된 자료형
+* 특정 영역에 있는     변수를 사용함
+*/
+class Player
+{
+    static public int m_gold = 0; // 저장공간을 만들고 값을 초기화함
+    static public int m_score = 1;//전역변수로 선언되지는 않았지만 전역변수처럼 사용
+
+}
+
+class Player2
+{
+    public int m_gold;
+    public int m_score;
+}
 
 namespace class_ex4
 {
@@ -10,6 +31,16 @@ namespace class_ex4
     {
         static void Main(string[] args)
         {
+            Player2 Player2;
+            //Player2.m_score = 100;
+            //Player2.m_gold = 100;
+
+            Console.WriteLine("값은" + Player.m_gold);
+            Player.m_gold += 100;
+            Player.m_score += 1000;
+            //영역, 변수 => 구조체나 포인터의 멤버느낌
+            //특정영역에 있는 변수를 사용.
+            Console.WriteLine("값은" + Player.m_gold+" 점수는:"+Player.m_score);
         }
     }
 }
