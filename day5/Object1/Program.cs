179b6cd6726c141980e1f997fab1da8e7de80e8d
kujin <dktmzdf@gmail.com>
2017-07-26 17:16:32 +0900

    Object1

---
+++ day5/Object1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Object1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object a = 123;
+            object b = 3.14159294618376417648723168971m;
+            object c = true;
+            object d = "안녕하세요";
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+        }
+    }
+}
