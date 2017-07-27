a8745e1f01233370c801fd7e93998a75a59013b8
kujin <dktmzdf@gmail.com>
2017-07-27 15:23:07 +0900

    inheritance_ex1

---
+++ day6/inheritance_ex1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance_ex1
+{
+    class Base
+    {
+        protected string Name;
+        public Base(string Name)
+        {
+            this.Name = Name;
+            Console.WriteLine("{0}.Base()",this.Name);
+        }
+
+        ~Base()
+        {
+            Console.WriteLine("{0}.~Base()", this.Name);
+        }
+        public void BaseMethod()
+        {
+            Console.WriteLine("{0}.BaseMethod()", this.Name);
+        }
+    }
+    sealed class Derived :Base
+    {
+        
+        public Derived(string Name) : base(Name)//(부모)this생성자 호출
+        {
+            this.Name = Name;
+            Console.WriteLine("{0}.Derived()", this.Name);
+        }
+
+        ~Derived()
+        {
+            Console.WriteLine("{0}.~Derived()", this.Name);
+        }
+        public void DerivedMethod()
+        {
+            Console.WriteLine("{0}.DerivedMethod()", this.Name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Base a = new Base("a");
+            a.BaseMethod();
+
+            Derived b = new Derived("b");
+            b.BaseMethod();
+            b.DerivedMethod();
+        }
+    }
+}
