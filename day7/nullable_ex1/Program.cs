f7843950b31cb1e406ffb2eb8a60b74c9aa5751f
kujin <dktmzdf@gmail.com>
2017-07-28 14:25:42 +0900

    nullable_ex1

---
+++ day7/nullable_ex1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nullable_ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? a = null;
+            Console.WriteLine(a.HasValue);
+            Console.WriteLine(a != null);
+
+            a = 3;
+            Console.WriteLine(a.HasValue);
+            Console.WriteLine(a !=null);
+            Console.WriteLine(a.Value);
+            
+
+            
+
+        }
+    }
+}
