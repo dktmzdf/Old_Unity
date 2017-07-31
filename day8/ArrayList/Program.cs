fb434a51dfb4f4e2ef28fed0a9227bf4d11f1fe9
kujin <dktmzdf@gmail.com>
2017-07-31 15:32:53 +0900

    ArrayList

---
+++ day8/ArrayList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+//같은 자료형의 집합: 1개 이상의 => collection
+//Arraylist = list 객체
+//HashTable => Dictionary(keyvalue 객체)
+//stack queue
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        ArrayList a = new ArrayList();
+            //생성 초기화
+            //데이터없는 최소한 공간
+            //a.add, a.Remove, a.Insert, a.RemoveAt, a.IndexOf, a.Clear, a.Count
+
+        }
+    }
+
