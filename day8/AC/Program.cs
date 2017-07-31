639d42634afa6e1383ef64ddb090de42909d0e70
kujin <dktmzdf@gmail.com>
2017-07-31 14:36:50 +0900

    AC

---
+++ day8/AC/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AC
+{
+    abstract class AbstractBase
+    {
+        protected void PrivateMethodA()
+        {
+            Console.WriteLine("AbstractBase.PrivateMethodA");
+        }
+        public void publicMethodA()
+        {
+            Console.WriteLine("AbstractBase.publicMethodA");
+        }
+
+        public abstract void AbstractMethodA();
+    }
+    class Derived :AbstractBase
+    {
+        public override void AbstractMethodA()
+        {
+            Console.WriteLine("AbstractBase.AbstractMethodA");
+            PrivateMethodA();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractBase obj = new Derived();
+            obj.AbstractMethodA();
+            obj.publicMethodA();
+        }
+    }
+}
