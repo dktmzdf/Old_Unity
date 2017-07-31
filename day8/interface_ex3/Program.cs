dc966e8d4919a7318c3d04ae7b5774b071d2c866
kujin <dktmzdf@gmail.com>
2017-07-31 14:37:27 +0900

    interface_ex3

---
+++ day8/interface_ex3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+interface IRunnable
+{
+    void Run();
+}
+
+interface IFlyalbe
+{
+    void Fly();
+}
+
+class FlyingCar : IRunnable, IFlyalbe
+{
+    public void Run()
+    {
+        Console.WriteLine("Run! Run");
+    }
+    public void Fly()
+    {
+        Console.WriteLine("Fly! Fly!");
+    }
+}
+
+namespace interface_ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FlyingCar car = new FlyingCar();
+            car.Run();
+            car.Fly();
+            ;
+            IRunnable runnable = car as IRunnable;
+            runnable.Run();
+
+            IFlyalbe flyable = car as IFlyalbe;
+            flyable.Fly();
+        }
+    }
+}
