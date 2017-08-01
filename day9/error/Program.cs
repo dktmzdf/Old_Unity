55defced835e02f263846512844ef0d4fdebe002
kujin <dktmzdf@gmail.com>
2017-08-01 14:41:24 +0900

    error

---
+++ day9/error/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace error
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+
+            for (int i =0; i<5; i++)
+            {
+                Console.WriteLine(arr[i]);
+
+
+            }
+            Console.WriteLine("종료");
+        }
+    }
+}
