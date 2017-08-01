2848f65b30a8390903be093266e51ebcd7dd88f1
kujin <dktmzdf@gmail.com>
2017-08-01 15:23:23 +0900

    trycatch_ex2

---
+++ day9/trycatch_ex2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trycatch_ex2
+{
+    
+    class Program
+    {
+        static void DoSomething(int arg)
+        {
+            if (arg < 10) Console.WriteLine("arg : {0}", arg);
+            else throw new Exception("10보다 크네");
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                DoSomething(1);
+                DoSomething(2);
+                DoSomething(6);
+                DoSomething(11);
+                DoSomething(21);
+                DoSomething(4);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
